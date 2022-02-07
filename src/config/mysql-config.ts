d7811fbd57ed9c117b84025e2b762238d04c8093
damtaipu <damiaotaipu@gmail.com>
2022-02-07 11:54:07 -0300

    Remove credentials

--- src/config/mysql-config.ts
+++ src/config/mysql-config.ts
@@ -2,10 +2,10 @@ import { createPool } from 'mysql';
 
 const pool = createPool({
     connectionLimit: 1000,
-    user: 'jdoliveira',
-    password: 'Damiao1981',
-    database: 'db-dev-instance-1',
-    host: 'database-dev.cy6uiwtkhc3t.sa-east-1.rds.amazonaws.com',
+    user: '***',
+    password: '**',
+    database: '**',
+    host: '***',
     port: 3306
 });
 
